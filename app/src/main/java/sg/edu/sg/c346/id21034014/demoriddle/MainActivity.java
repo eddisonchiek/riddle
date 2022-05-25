699d569edd2a5c259d2f20bef817794483b47e3f
eddisonchiek <21034014@myrp.edu.sg>
2022-05-25 20:10:59 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/sg/c346/id21034014/demoriddle/MainActivity.java
@@ -0,0 +1,90 @@
+package sg.edu.sg.c346.id21034014.demoriddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvQ1;
+    Button btnRevealQ1;
+    Button btnRevealQ2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvQ1 = findViewById(R.id.textViewQ1);
+        btnRevealQ1 = findViewById(R.id.buttonRevealQ1);
+        btnRevealQ2 = findViewById(R.id.buttonRevealQ2);
+
+        btnRevealQ1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AnswerActivity1.class);
+                startActivity(intent);
+            }
+        });
+        btnRevealQ1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AnswerActivity1.class);
+                intent.putExtra("Question", "Q1");
+                startActivity(intent);
+            }
+        });
+        btnRevealQ2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AnswerActivity2.class);
+                startActivity(intent);
+            }
+        });
+        btnRevealQ2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AnswerActivity2.class);
+                intent.putExtra("Question", "Q2");
+                startActivity(intent);
+            }
+        });
+        @Override
+        protected void onStart() {
+            Log.d("MainActivity", "onStart() called.");
+            super.onStart();
+        }
+        @Override
+        protected void onResume() {
+            Log.d("MainActivity", "onResume() called.");
+            super.onResume();
+        }
+        @Override
+        protected void onPause() {
+            Log.d("MainActivity", "onPause() called.");
+            super.onPause();
+        }
+        @Override
+        protected void onStop() {
+            Log.d("MainActivity", "onStop() called.");
+            super.onStop();
+        }
+        @Override
+        protected void onDestroy() {
+            Log.d("MainActivity", "onDestroy() called.");
+            super.onDestroy();
+        }
+        @Override
+        protected void onRestart() {
+            Log.d("MainActivity", "onRestart() called.");
+            super.onRestart();
+        }
+
+
+
+    }
+}
